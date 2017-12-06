aecb7a75a5e77eb579a5bbcac422dc471ea06c1d
ValentineGladyshko <valentine9022@gmail.com>
2017-12-06 22:09:36 +0200

    Added page RescueService and Fixed bugs with website layout.

---
+++ Coursework/Coursework/Controllers/AdministratingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Coursework.Controllers
+{
+    public class AdministratingController : Controller
+    {
+        // GET: Administrating
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Coursework/Coursework/Controllers/RescueServiceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Coursework.Controllers
+{
+    public class RescueServiceController : Controller
+    {
+        // GET: RescueService
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Information()
+        {
+            return View();
+        }
+    }
+}
